171d749dcbdd8c25264276b43daafd0a84140a86
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2016-09-26 09:42:12 +0200

    Pruebas bien

--- PruebasPrimitiva.sql
+++ PruebasPrimitiva.sql
@@ -2,5 +2,7 @@ Use Primitiva
 go
 Insert into Sorteo(IdSorteo,FechaSorteo)
 Values (5,CURRENT_TIMESTAMP)
-
-Execute GrabaSencilla 5, 1,2,3,4,5,6
+declare @IdBoleto bigint
+Execute GrabaSencilla 5, 1,2,3,4,5,6,@IdBoleto
+Select * from Boleto
+Select * from NumeroBoleto
